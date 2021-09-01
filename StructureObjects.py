e4abdf081e67d37c58dfe7102611153b180f117b
expiredy101 <yartaoley2005@mail.ru>
2021-09-01 23:29:11 +0300

    And here we have another AI project about speed dating (don't ask me why) Let's get this crazy party started

---
+++ StructureObjects.py
@@ -0,0 +1,8 @@
+class UnitsDataHolder:
+    unique_subject_number = None  #iid
+    subject_id_in_wave = None  #id
+    gender_id = None  #gender
+    subject_id_within_gender = None  #idg
+    condition_of_choice = None  #condtn
+    dating_wave_id = None  #wave
+    number_of_met_people = None  #round
---
+++ main.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy
+import requests
+import matplotlib.pyplot as plt
+
+
+RANDOM_NP = numpy.random
+TESTING_DATA_SET_PATH = "SpeedDatingData.csv"
+
+
+def get_data_from_excel_file(data_path):
+    import pandas
+    parsed_data = pandas.read_csv(data_path)
+    return parsed_data
+
+
+
+
+data_set = get_data_from_excel_file(TESTING_DATA_SET_PATH)
