8ab0ad7947d71962236cdcc14e44ca9089e35a13
expiredy101 <yartaoley2005@mail.ru>
2021-09-02 11:45:39 +0300

    Oh... U may ask "What new y developed and wanna show to this world?" and i ask u: "ABSOLUTELY NOTHING", cause i still don't get how this data_set works

--- StructureObjects.py
+++ StructureObjects.py
@@ -20,6 +20,46 @@ class UnitsDataHolder:
     decision_of_partner_night_of_even = None  #dec_o
     rating_by_partner_night_of_even = None  #attr_o
 
+    age = None  #JUST FRICKING "AGE" I HAVE NO IDEA WHAT AND WHY AND HOW AND FOR WHAT IT'S GOES ON
+    name_of_field_of_study = None  #field
+    coded_field_of_study_id = None  #field_cd
+    school_grade = None  #undergrd
+    median_SAT_score = None  #mn_sat
+    tuition_listed_for_each_response_to_undergrad = None  #tuition
+    human_race_number = None  #WHY YOU NIBBA???? race
+    how_much_caring_for_race = None  #imprace
+    how_much_caring_for_religious = None  #imprelig
+    native_country = None  #from
+    post_code_od_your_native_country = None  #zipcode
+    median_household_based_on_zipcode = None  #income
+    what_is_your_purpose_id = None  #goal
+    dating_rate_id = None  #date
+    how_often_go_out_id = None  #go out
+    career_name = None  #career
+    coded_career_name_id = None  #career_c
+
+    # How interested are unit in the following activities
+    sports = None
+    tvsports = None
+    excersice = None
+    dining = None
+    museums = None
+    art = None
+    hiking = None
+    gaming = None
+    clubbing = None
+    reading = None
+    tv = None
+    theater = None
+    movies = None
+    concerts = None
+    music = None
+    shopping = None
+    yoga = None
+
+    rate_your_happiness_expectations = None  #exphappy
+    rate_expectation_number_of_people_who_will_dating_unit = None  #expnum
+
 
 class DatingWaveDataHolder:
     pass
