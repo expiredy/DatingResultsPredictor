99babeb03377c2d19beee35edfa94c6b77c74864
expiredy101 <yartaoley2005@mail.ru>
2021-09-02 11:02:45 +0300

    So what we have here... i am just trying to come up with how 2 call parameters of average unit, cause i have a fricking strange database (which btw doesn't want to share data with me) and i still have no idea which parameter is responsible for what... Have a nice day, strange person who reading this commits

--- StructureObjects.py
+++ StructureObjects.py
@@ -6,3 +6,20 @@ class UnitsDataHolder:
     condition_of_choice = None  #condtn
     dating_wave_id = None  #wave
     number_of_met_people = None  #round
+    met_place_position_number = None  #position
+    dating_start_position_number = None  #positin1
+    number_of_data_that_night = None  #order
+    partner_id_by_night_of_events = None  #partner
+    iid_number_of_number = None  #pid
+    is_match_choice = None  #match
+    correlation_between_participant_and_partner_ratings_of_interests_time_1 = None  #int_corr
+    is_partners_have_same_rice_choice = None  #samerace
+    age_of_partner = None  #age_o
+    race_of_partner = None  #race_o
+    preferences_of_partner = None  #pf_o_att
+    decision_of_partner_night_of_even = None  #dec_o
+    rating_by_partner_night_of_even = None  #attr_o
+
+
+class DatingWaveDataHolder:
+    pass
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 RANDOM_NP = numpy.random
 TESTING_DATA_SET_PATH = "SpeedDatingData.csv"
 
+LEARNING_RATE = 0.02
+TRAINING_EPOCHS = 3000
+
+display_step = 50
 
 def get_data_from_excel_file(data_path):
     import pandas
